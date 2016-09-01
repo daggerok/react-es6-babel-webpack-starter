80f4d9d200bc69b614faeeb39616887005745213
Maksim Kostromin <daggerok@gmail.com>
2016-09-01 03:56:54 +0300

    add jquery componend did mount fetch mocked api

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 export class Home extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'world'
-    }
+      name: 'world',
+      users: []
+    };
+    this.fetchUsers = this.fetchUsers.bind(this);
+  }
+
+  fetchUsers() {
+    $.get('./api/data.json')
+      .then(result => {
+        this.setState(Object.assign({}, this.state, {
+          users: result
+        }))
+      });
+  }
+
+  componentDidMount() {
+    this.fetchUsers()
   }
 
   render() {
     return (
-      <div>
+      <div onClick={this.fetchUsers}>
         hello, {this.state.name}!
+        <ul>
+          {this.state.users.map((user, index) => <li key={index}>{user.name}</li>)}
+        </ul>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new CopyWebpackPlugin([
+      { from: './api', to: './dist/api' }
+    ]),
     new HtmlWebpackPlugin({
       title: 'react-es6-babel-webpack-starter',
       template: './src/assets/index.html'
